7761fec8aa030365221d4b3d03fa50f72fea6c71
Lucas Gomide <lucaslg200@gmail.com>
2016-08-09 21:03:34 -0300

    Add DotIt project

---
+++ Android/Doit/app/src/main/java/com/example/rm71058/doit/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.rm71058.doit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Toast.makeText(this, "Efetuando login", Toast.LENGTH_SHORT).show();
+    }
+}
